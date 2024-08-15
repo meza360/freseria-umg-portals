efc3d715df9cd2bb62d8cc4fb52b794049eee3fb
Giovani Meza <giovani.meza@gt2software.dev>
2024-08-14 23:31:55 -0600

    Environment

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:7071',
+    apiUrl: 'https://gateway-01.api.gt2software.dev/ecommerce',
     productCatalog: [],
-    functionKey: '',
-    apimKey: ''
+    functionKey: 't_GRcwvfVgFxuoSZO2D5Z92QTghs8T6vdyZ779oxeWU_AzFurM-LuA==',
+    apimKey: '9c756c1ea86a450b82a9a1eed67449aa'
 };
