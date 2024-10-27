74af005344dffc3ed77de9d7a92e52ee2a8965e3
Giovani Meza <giovani.meza@gt2software.dev>
2024-10-27 08:30:28 -0600

    Adds routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './pages/ecommerce/cart/cart.component';
+import { OrderComponent } from './pages/ecommerce/order/order.component';
 
 const routes: Routes = [
   {
     path: 'ecommerce/cart',
     component: CartComponent
+  },
+  {
+    path: "",
+    redirectTo: "ecommerce/cart", pathMatch: 'full'
+  },
+  {
+    path: 'ecommerce/order/:id',
+    component: OrderComponent
   }
-  /* {
-    //path: "",
-    //redirectTo: "/", pathMatch: 'full'
-  } */
 ];
 
 @NgModule({
